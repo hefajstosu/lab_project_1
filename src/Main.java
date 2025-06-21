c6c61df387bb127107ec37c6231fb9124ba2b87e
Bartosz Wilkosz <bartosz@wilkosz.com>
2025-06-21 08:52:42 +0200

    did 1st persons task - clearing interpunction

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String content = Files.readString(Paths.get("src/text_to_clean.txt"));
 
         String trimmed = Arrays.stream(content.split("\n"))
@@ -12,5 +14,19 @@ public class Main {
                 .skip(1)
                 .takeWhile(line -> !line.contains("*** END OF THIS PROJECT GUTENBERG EBOOK"))
                 .collect(Collectors.joining(" "));
+
+
+
+        Function<String, String> cleanText = text -> text
+                .replaceAll("[.,\"'!?;:()\\[\\]\\-–—]", "")
+                .toLowerCase()
+                .replaceAll("\\s+", " ")
+                .trim();
+
+
+        String cleaned = cleanText.apply(trimmed);
+
+
     }
+
 }

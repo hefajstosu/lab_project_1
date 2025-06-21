96be1cd2440dd63b9524b7dad279f8a5cfa484ba
szymonkunysz11 <279630@student.pwr.edu.pl>
2025-06-21 08:39:46 +0200

    read text and delete headers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String content = Files.readString(Paths.get("src/text_to_clean.txt"));
+
+        String trimmed = Arrays.stream(content.split("\n"))
+                .dropWhile(line -> !line.contains("*** START OF THIS PROJECT GUTENBERG EBOOK"))
+                .skip(1)
+                .takeWhile(line -> !line.contains("*** END OF THIS PROJECT GUTENBERG EBOOK"))
+                .collect(Collectors.joining(" "));
     }
 }
